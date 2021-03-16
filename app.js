07990b694832657483feca86eddb5f249c277de6
David Wu <david204code@gmail.com>
2021-03-16 22:40:40 +0000

    JavaScript date example, set up the date

--- app.js
+++ app.js
@@ -27,3 +27,27 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 // console.log(items);
+
+// date= year/months(0 index based), date, hours, minutes, seconds
+let futureDate = new Date(2021, 5, 01, 18, 10, 0);
+// console.log(futureDate);
+
+// extract the year
+const year = futureDate.getFullYear();
+// extract the hours
+const hours = futureDate.getHours();
+// extract the minutes
+const minutes = futureDate.getMinutes();
+// month 0 index based
+let month = futureDate.getMonth();
+// console.log(months[month]);
+month = months[month];
+// extract date
+const date = futureDate.getDate();
+// console.log(date);
+// day...getDate 0 to 6 Sunday===0
+const weekday = weekdays[futureDate.getDay()];
+// console.log(weekdays[weekday]);
+
+// functionality to update the year in the HTML
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}pm`;
